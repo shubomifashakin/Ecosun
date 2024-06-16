7fa68383986609541949e75366d9d513205cd838
Fashakin Olashubomi <subomifasakin@outlook.com>
2024-06-16 07:55:25 +0100

    bug fixes

--- src/js/landing.js
+++ src/js/landing.js
@@ -27,15 +27,41 @@ class Page {
   catalogRightArrow = this.shopSect.querySelector("#scroll-right-btn");
 
   constructor() {
-    this.catalogRightArrow.addEventListener(
-      "click",
-      this.scrollCatalog.bind(this, "right"),
+    //scroll right every 3 seconds
+    this.intervalId = setInterval(
+      this.scrollTestimonial.bind(this, "right"),
+      3500,
     );
 
-    this.catalogLeftArrow.addEventListener(
-      "click",
-      this.scrollCatalog.bind(this, "left"),
-    );
+    //when a dot is clicked, scroll the associated article into view
+    this.testimonialDots.forEach((dot, i) => {
+      dot.addEventListener("click", (e) => {
+        const articleNo = e.target.dataset.articleNo;
+
+        //if the dot clicked is greater than the one currently selected,
+        //scroll the section by the distance between the currently selected dot and the newly selected one
+        this.testimonialCustomers.scrollBy({
+          left:
+            articleNo > this.currentTestimonialInView
+              ? this.testimonialCustomers.clientWidth *
+                (articleNo - this.currentTestimonialInView)
+              : -this.testimonialCustomers.clientWidth *
+                (this.currentTestimonialInView - articleNo),
+          behavior: "smooth",
+        });
+
+        //add the active class to the dot clicked
+        dot.classList.add("bg-primaryBlue");
+
+        //remove the active background color from the previously selected dot
+        this.testimonialDots.forEach((dot2, i2) => {
+          i !== i2 ? dot2.classList.remove("bg-primaryBlue") : null;
+        });
+
+        //change the currently selected testimonial value
+        this.currentTestimonialInView = articleNo;
+      });
+    });
 
     //if anywhere on services is clicked
     //close all the open details except the one that was clicked
@@ -50,25 +76,15 @@ class Page {
       });
     });
 
-    //when a dot is clicked, scroll the associated article into view
-    this.testimonialDots.forEach((dot, i) => {
-      dot.addEventListener("click", (e) => {
-        this.testimonialCustomers
-          .querySelector(`#article-${e.target.dataset.articleNo}`)
-          .scrollIntoView({ behavior: "instant" });
+    this.catalogRightArrow.addEventListener(
+      "click",
+      this.scrollCatalog.bind(this, "right"),
+    );
 
-        //add the active class to the dot clicked
-        dot.classList.add("bg-primaryBlue");
-
-        //remove the classlist from the dot
-        this.testimonialDots.forEach((dot2, i2) => {
-          i !== i2 ? dot2.classList.remove("bg-primaryBlue") : null;
-        });
-
-        //change the currently selected dot value
-        this.currentTestimonialInView = e.target.dataset.articleNo;
-      });
-    });
+    this.catalogLeftArrow.addEventListener(
+      "click",
+      this.scrollCatalog.bind(this, "left"),
+    );
 
     this.testimonialRightArrow.addEventListener(
       "click",
@@ -102,18 +118,45 @@ class Page {
   }
 
   scrollTestimonial(dir) {
-    dir === "right"
-      ? this.currentTestimonialInView < 5
-        ? this.currentTestimonialInView++
-        : (this.currentTestimonialInView = 1)
-      : this.currentTestimonialInView > 1
-        ? this.currentTestimonialInView--
-        : (this.currentTestimonialInView = 5);
+    //if the user clicked right and we have not reached the end
+    //go forward one div
+    dir === "right" && this.currentTestimonialInView <= 5
+      ? (this.currentTestimonialInView++,
+        this.testimonialCustomers.scrollBy({
+          left: this.testimonialCustomers.clientWidth,
+          behavior: "smooth",
+        }))
+      : null;
 
-    //scroll associated element into view
-    this.testimonialCustomers
-      .querySelector(`#article-${this.currentTestimonialInView}`)
-      .scrollIntoView({ behavior: "instant" });
+    //if the user clicked right and we have reached the end
+    //go back to the beginning
+    dir === "right" && this.currentTestimonialInView > 5
+      ? ((this.currentTestimonialInView = 1),
+        this.testimonialCustomers.scrollBy({
+          left: -this.testimonialCustomers.clientWidth * 5,
+          behavior: "smooth",
+        }))
+      : null;
+
+    //if the user clicked left and we have not reached the beginning
+    //go back one div
+    dir === "left" && this.currentTestimonialInView >= 1
+      ? (this.currentTestimonialInView--,
+        this.testimonialCustomers.scrollBy({
+          left: -this.testimonialCustomers.clientWidth,
+          behavior: "smooth",
+        }))
+      : null;
+
+    //if the user clicked left and we have reached the beginning
+    //scroll to the end
+    dir === "left" && this.currentTestimonialInView < 1
+      ? ((this.currentTestimonialInView = 5),
+        this.testimonialCustomers.scrollBy({
+          left: this.testimonialCustomers.clientWidth * 5,
+          behavior: "smooth",
+        }))
+      : null;
 
     //color the associated dot
     this.testimonialDots.forEach((c, i) => {
